de83640bbe27e303a4a4d8739e4c31f29cf0ba2d
Jason E <ensminger@yahoo.com>
2022-09-06 11:27:00 -0400

    Update mm-setup.sh

--- mm-setup.sh
+++ mm-setup.sh
@@ -150,6 +150,7 @@ cp conf.json ~/script-server/conf
 cp control.sh ~/script-server
 sudo chmod +x ~/script-server/control.sh
 pip3 install -r requirements.txt
+python3 ~/script-server/tools/init.py --no-npm
 pm2 start ~/script-server/control.sh
 sleep 5
 pm2 save
