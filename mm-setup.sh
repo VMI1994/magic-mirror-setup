c462a6a7a96ad388b2068d138729aa4d93347a8d
Jason E <ensminger@yahoo.com>
2023-09-13 10:36:59 -0400

    Update mm-setup.sh

--- mm-setup.sh
+++ mm-setup.sh
@@ -52,7 +52,7 @@ fi
 # Install node.js
 echo "We will now install node.js"
 sleep 5
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt update
 sudo apt install -y nodejs
 
